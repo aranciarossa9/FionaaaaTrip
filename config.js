70e460156ed2c99cefa62e21b7c889a32cae2063
aranciarossa9 <aranciarossa9@gmail.com>
2026-05-16 16:00:29 +0200

    Changes Stile 4

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
   // Ottieni queste credenziali su https://jsonbin.io
   JSONBIN_MASTER_KEY: '$2a$10$QDlRvEa3J65AnpCQxKN4VOE1lJbi02EFNcAWC9hmr6kXIdaqlQP9O',
   BIN_ID:             '6a08540dc0954111d8309c8c',
-  VOTING_START_DATE:  '2025-10-24T00:00:00', // ISO date in UTC or local time, cambia con la data esatta
+  VOTING_START_DATE:  '2026-05-24T00:00:00', // ISO date in UTC or local time, cambia con la data esatta
 
   // Modifica con il numero esatto di persone che voteranno
   // I risultati si sbloccano solo quando tutti hanno votato
