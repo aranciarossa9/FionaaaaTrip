32f5e04fee8659555e074f52b864382ea9ed02e8
aranciarossa9 <aranciarossa9@gmail.com>
2026-05-16 17:45:04 +0200

    Test Vote

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ const CONFIG = {
   // Ottieni queste credenziali su https://jsonbin.io
   JSONBIN_MASTER_KEY: '$2a$10$QDlRvEa3J65AnpCQxKN4VOE1lJbi02EFNcAWC9hmr6kXIdaqlQP9O',
   BIN_ID:             '6a08540dc0954111d8309c8c',
-  VOTING_START_DATE:  '2026-05-24T00:00:00', // ISO date in UTC or local time, cambia con la data esatta
+  VOTING_START_DATE:  '2026-05-15T00:00:00', // ISO date in UTC or local time, cambia con la data esatta
 
   // Modifica con il numero esatto di persone che voteranno
   // I risultati si sbloccano solo quando tutti hanno votato
-  TOTAL_VOTERS: 10,
+  TOTAL_VOTERS: 11,
 };
